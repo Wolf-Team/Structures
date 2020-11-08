858105ad340a75903a289a1a1994a5efe3ed3fde
80LK <80lkru@gmail.com>
2020-11-08 20:25:44 +0300

    Generate
    - Add test generation in chunk
    - Add default BlockSourse
    
    Test structure by
    
    Co-Authored-By: DansZbar2 <40437965+DansZbar2@users.noreply.github.com>

---
+++ dev/tests/gen_chunk.js
@@ -0,0 +1,14 @@
+let struct = new Structure("gen_chunk");
+
+Callback.addCallback("GenerateChunk", function(chunkX, chunkZ, random, dimensionId) {
+    if (random.nextFloat() < .25){
+        
+        var coords = GenerationUtils.randomCoords(chunkX, chunkZ);
+        coords = GenerationUtils.findSurface(coords.x, 95, coords.z);
+        if(coords.y < 52) return;
+
+        //let region = BlockSource.getCurrentWorldGenRegion();
+        
+        struct.build(coords.x, coords.y + 1, coords.z, Structure.ROTATE_Y, random /*, region */ );
+    }
+});
--- devlib/structure.js
+++ devlib/structure.js
@@ -6,6 +6,8 @@ var SUPPORT_NETWORK = getCoreAPILevel() > 10;
 var _World = {
     setBlock:function(x,y,z, id, data, blockSource){
         if(SUPPORT_NETWORK){
+            if(!blockSource)
+                blockSource = BlockSource.getCurrentWorldGenRegion();
             blockSource.setBlock(x,y,z, id, data);
         }else{
             World.setBlock(x,y,z, id, data);
@@ -13,6 +15,8 @@ var _World = {
     },
     getBlock:function(x, y, z, blockSource){
         if(SUPPORT_NETWORK){
+            if(!blockSource)
+                blockSource = BlockSource.getCurrentWorldGenRegion();
             return blockSource.getBlock(x, y, z);
         }else{
             return World.getBlock(x, y, z);
